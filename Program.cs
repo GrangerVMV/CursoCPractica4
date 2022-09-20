f0e516fdaa3524f655fc1ce4e32d28c0b2bc1cf0
GrangerVMV <javitrunk@hotmail.com>
2022-09-20 17:44:42 +0200

    Segundo ejemplo: Uso de bucle While para quiz adivinar número

--- Program.cs
+++ Program.cs
@@ -19,6 +19,21 @@ namespace CursoCPractica4_Bucles
                 respuesta = Console.ReadLine();
             }
             Console.WriteLine("Información: Has salido del bucle While");
+
+            // Segundo ejemplo: Uso de bucle While para quiz adivinar número
+            Random numero = new Random();
+            int numeroAleatorio = numero.Next(0,100);    
+            Console.WriteLine("Por favor, elige un número del 0-100");
+            int numeroElegido = int.Parse(Console.ReadLine());
+            int numIntentos = 1;
+            while (numeroElegido != numeroAleatorio) 
+            {
+                if (numeroAleatorio > numeroElegido) Console.WriteLine("Vuelve a intentarlo, el número es mayor que el que has elegido");
+                else if (numeroAleatorio < numeroElegido) Console.WriteLine("Vuelve a intentarlo, el número es menor que el que has elegido");
+                numeroElegido = int.Parse(Console.ReadLine());
+                numIntentos = numIntentos + 1;                              
+            }
+            Console.WriteLine($"Enhorabuena, el número secreto era el {numeroAleatorio} y has acertado en {numIntentos} intentos");
         }
     }
 }
